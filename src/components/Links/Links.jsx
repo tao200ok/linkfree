e499946be7e661ca9e3e84a3a5b4f201dd907f03
Tao. H <hamzatdestiny@gmail.com>
2022-11-01 06:28:30 +0100

    Improve Layout

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -3,14 +3,15 @@ import Link from '../Link/Link';
 
 function Links() {
     return (
-        <>
+        <article>
+            <Link id="slack" url="#" content="tao" />
             <Link id="twitter" url="https://twitter.com/gtktao" content="Twitter Link" />
             <Link id="btn_zuri" url="https://training.zuri.team" content="Zuri Team" />
             <Link id="books" url="https://books.zuri.team" content="Zuri Books" />
             <Link id="book__python" url="https://books.zuri.team/python-for-beginners?ref_id=tao" content="Python Books" />
             <Link id="pitch" url="#" content="Background Check for Coders" />
             <Link id="book__design" url="https://books.zuri.team/design-rules" content="Design Books" />
-        </>
+        </article>
     );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,13 +2,19 @@ import './Profile.css';
 
 // import Link from '../Link/Link';
 
+import shareIcon from '../../assets/share_icon.svg';
+
 import profileImage from '../../assets/profile__img.svg'
 
 function Profile() {
     return (
         <header>
             <img src={profileImage} alt="user" id="profile__image" />
-            <h1>First Last</h1>
+            <h1>Taofeeq Hamzat</h1>
+            <div class="share">
+                <span class="tooltiptext">Share Link</span>
+                <img src={shareIcon} alt="share" id="share" />
+            </div>
         </header>
     );
 }
