1eb55159e5e2206bcf249c48977233804f06d898
Tao. H <hamzatdestiny@gmail.com>
2022-11-01 11:12:28 +0100

    add copy-to-clipboard functionality

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,15 +4,15 @@ import './Profile.css';
 
 import shareIcon from '../../assets/share_icon.svg';
 
-import profileImage from '../../assets/profile__img.jpeg'
+import profileImage from '../../assets/profile__img.jpeg';
 
 function Profile() {
     return (
         <header>
             <img src={profileImage} alt="user" id="profile__image" />
             <h1>Taofeeq Hamzat</h1>
-            <div class="share">
-                <span class="tooltiptext">Share Link</span>
+            <div class="share" onClick={()=> navigator.clipboard.writeText("https://twitter.com/gtktao")}>
+                <span class="tooltiptext">Copy Link</span>
                 <img src={shareIcon} alt="share" id="share" />
             </div>
         </header>
