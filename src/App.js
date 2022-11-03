45e9534f7b222579e5bfdaab34f68c3986b10d3a
Tao. H <hamzatdestiny@gmail.com>
2022-11-03 22:23:12 +0100

    feat: Implement client-side routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
+import { Route, Routes } from 'react-router-dom';
+
 import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </ Routes>
       </main>
       <Footer />
     </div>
--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,16 +1,18 @@
 import './Links.css';
 import Link from '../Link/Link';
+import { Link as RouterLink } from 'react-router-dom';
 
 function Links() {
     return (
         <article>
             <Link id="slack" url="#" content="tao" />
             <Link id="twitter" url="https://twitter.com/gtktao" content="Twitter Link" />
-            <Link id="btn_zuri" url="https://training.zuri.team" content="Zuri Team" />
+            <Link id="btn__zuri" url="https://training.zuri.team" content="Zuri Team" />
             <Link id="books" url="https://books.zuri.team" content="Zuri Books" />
             <Link id="book__python" url="https://books.zuri.team/python-for-beginners?ref_id=tao" content="Python Books" />
             <Link id="pitch" url="#" content="Background Check for Coders" />
             <Link id="book__design" url="https://books.zuri.team/design-rules" content="Design Books" />
+            <RouterLink id="contact" className="link" to="/contact">Contact Me</RouterLink>
         </article>
     );
 }
