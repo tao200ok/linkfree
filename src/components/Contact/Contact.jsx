b1faf620cc5e7d416daee5ef9f784b1d66c828ae
Tao. H <hamzatdestiny@gmail.com>
2022-11-03 21:01:22 +0100

    chore: Update with task's required ids

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,29 +8,29 @@ function Contact() {
                 <p>Hi there, contact me to ask me about anything you have in mind.</p>
             </header>
             <form action="./">
-                <div id="form__name">
-                    <div className="name-field-container">
-                        <label htmlFor="first-name">First name</label>
+                <div id="name">
+                    <div className="name_field-container">
+                        <label htmlFor="first_name">First name</label>
                         <br />
-                        <input type="text" placeholder="Enter your first name" id="first-name" />
+                        <input type="text" placeholder="Enter your first name" id="first_name" />
                     </div>
-                    <div className="name-field-container">
-                        <label htmlFor="last-name">Last name</label>
+                    <div className="name_field-container">
+                        <label htmlFor="last_name">Last name</label>
                         <br />
-                        <input type="text" placeholder="Enter your last name" id="last-name" />
+                        <input type="text" placeholder="Enter your last name" id="last_name" />
                     </div>
                 </div>
-                <label htmlFor="form__email">Email</label>
+                <label htmlFor="email">Email</label>
                 <br />
-                <input type="email" placeholder="yourname@email.com" id="form__email" />
-                <label htmlFor="form__message">Message</label>
+                <input type="email" placeholder="yourname@email.com" id="email" />
+                <label htmlFor="message">Message</label>
                 <br />
-                <textarea placeholder="Send me a message and I'll reply you as soon as possible..." id="form__message" />
+                <textarea placeholder="Send me a message and I'll reply you as soon as possible..." id="message" />
                 <div id="consent-container">
-                    <input type="checkbox" id="form__consent-checkbox" />
-                    <label htmlFor="form__consent-checkbox">You agree to providing your data to Taofeeq Hamzat who may contact you.</label>
+                    <input type="checkbox" id="consent-checkbox" />
+                    <label htmlFor="consent-checkbox">You agree to providing your data to Taofeeq Hamzat who may contact you.</label>
                 </div>
-                <input type="submit" value="Send message" />
+                <input type="submit" id="btn__submit" value="Send message" />
             </form>
         </article>
     )
