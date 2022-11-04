01d53a52542ff75253b34660995025804e214f09
Tao. H <hamzatdestiny@gmail.com>
2022-11-04 17:22:54 +0100

    feat: Add feedback to share button

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -5,12 +5,24 @@ import shareIcon from '../../assets/share_icon.svg';
 import profileImage from '../../assets/profile__img.jpeg';
 
 function Profile() {
+    const copyLink = () => {
+        const tooltip = document.getElementsByClassName("tooltiptext")[0];
+        navigator.clipboard.writeText("https://twitter.com/gtktao").then(() => {
+        tooltip.style.visibility = "visible";
+        tooltip.innerText = "Link copied to clipboard!";
+        setTimeout(() => {
+            tooltip.innerText = "Copy link";
+            tooltip.style.visibility = "hidden";
+        }, 1000);
+        });
+    };
+
     return (
         <header>
             <img src={profileImage} alt="user" id="profile__image" />
             <h1>Taofeeq Hamzat</h1>
-            <div class="share" onClick={()=> navigator.clipboard.writeText("https://twitter.com/gtktao")}>
-                <span class="tooltiptext">Copy Link</span>
+            <div className="share" onClick={copyLink}>
+                <span className="tooltiptext">Copy link</span>
                 <img src={shareIcon} alt="share" id="share" />
             </div>
         </header>
